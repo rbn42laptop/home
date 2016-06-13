f2921077e424a80a3de9fcd839f0fc217d5b603a
rbn42 <bl100@students.waikato.ac.nz>
2016-06-13 14:48:52 +1200

    auto commit

---
+++ bin/keys.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#setxkbmap -option "caps:swapescape"
+setxkbmap -option "caps:escape"
+#setxkbmap -option "ctrl:swapcaps"
+#setxkbmap -model "hhk"
+#http://hack.org/mc/writings/xkb.html
