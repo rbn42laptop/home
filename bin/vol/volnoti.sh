166a2498188eb57c51f9b4760509c24d102cf768
rbn42 <bl100@students.waikato.ac.nz>
2016-07-02 06:51:57 +1200

    auto commit

--- bin/vol/volnoti.sh
+++ bin/vol/volnoti.sh
@@ -3,12 +3,12 @@ export MUTE=`pamixer --get-mute`
 export VOL=`pamixer --get-volume`
 if [ "false" == $MUTE ];
 then
-    #volnoti-show $VOL
+    volnoti-show $VOL
     #图太大了,位置不好,干扰性太强.
     #notify-send $VOL
     echo $VOL
 else
-    #volnoti-show $VOL -m
+    volnoti-show $VOL -m
     echo $VOL
 fi
 
