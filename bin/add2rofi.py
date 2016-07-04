8dd2411da748ac3f500dbe007126ce91689d0bb7
rbn42 <bl100@students.waikato.ac.nz>
2016-07-05 03:19:21 +1200

    auto commit

--- bin/add2rofi.py
+++ bin/add2rofi.py
@@ -1,15 +1,21 @@
-import sys
 import getpass
-username = getpass.getuser()
-rofi = sys.argv[1]
-cmds = list(sys.argv[2:4])
-args = sys.argv[4:]
-for s in args:
-    if not s.startswith('-'):
-        s = s.replace('/%s/' % username, '/$USER/')
-        s = '"%s"' % s
-    cmds.append(s)
-cmd = ' '.join(cmds)
-f = open(rofi, 'a')
-f.write(cmd + '\n')
-f.close()
+
+
+def add(rofi, cmds, args):
+    username = getpass.getuser()
+    for s in args:
+        if not s.startswith('-'):
+            s = s.replace('/%s/' % username, '/$USER/')
+            s = '"%s"' % s
+        cmds.append(s)
+    cmd = ' '.join(cmds)
+    f = open(rofi, 'a')
+    f.write(cmd + '\n')
+    f.close()
+
+if __name__ == '__main__':
+    import sys
+    rofi = sys.argv[1]
+    cmds = list(sys.argv[2:4])
+    args = sys.argv[4:]
+    add(rofi, cmds, args)
