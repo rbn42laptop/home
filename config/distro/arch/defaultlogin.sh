260a994cf7eca5ea484412331111271c6a0f1c9a
rbn42 <bl100@students.waikato.ac.nz>
2016-07-08 22:32:58 +1200

    auto commit

--- config/distro/arch/defaultlogin.sh
+++ config/distro/arch/defaultlogin.sh
@@ -1,3 +1,4 @@
+export EDITOR=vim
 sudo systemctl edit getty@tty1
 
 """
@@ -5,3 +6,6 @@ sudo systemctl edit getty@tty1
 ExecStart=
 ExecStart=-/usr/bin/agetty --autologin username --noclear %I $TERM
 """
+
+#有必要开两个tty,因为第一个tty作为gui,可能崩溃,需要第二个tty从命令行恢复.
+sudo systemctl edit getty@tty2
