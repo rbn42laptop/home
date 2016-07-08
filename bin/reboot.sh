4390b9ce5e9c5e6205748fe9d7f0dfc3ab1e955a
rbn42 <bl100@students.waikato.ac.nz>
2016-07-09 00:41:14 +1200

    auto commit

--- bin/reboot.sh
+++ bin/reboot.sh
@@ -3,10 +3,15 @@ pkill mpd
 pkill mpv
 pkill firefox
 pkill thunderbird
-pkill wine  
 pkill dolphin
+pkill wine  
+
 pkill kactivity
 pkill gam_serve
+
+pkill conky
+pkill docky
+pkill xfce4-panel
 
 udisksctl unmount -b /dev/disk/by-label/dmzj 
 udisksctl unmount -b /dev/disk/by-label/4T
