a59e2dc55f75f65d23f7908966ed8c36cdb37506
rbn42 <bl100@students.waikato.ac.nz>
2016-07-13 20:30:45 +1200

    auto commit

--- bin/localsync.sh
+++ bin/localsync.sh
@@ -8,3 +8,8 @@ rsync -a --delete-excluded $HOME/laosao                 /media/$USER/dmzj/sync_d
 rsync -a --delete-excluded $HOME/Pictures/compiz/raw    /media/$USER/dmzj/sync_del
 rsync -a --delete-excluded $HOME/.mozilla                 /media/$USER/dmzj/sync_del
 rsync -a --delete-excluded $HOME/.thunderbird           /media/$USER/dmzj/sync_del
+
+#auto rsync arch linux wiki 
+#https://wiki.archlinux.org/index.php/Syncthing
+#https://en.wikipedia.org/wiki/Comparison_of_online_backup_services
+#cloud不太好,因为基本都是商业软件,依赖限制都很多,处理起来很麻烦.
