39d9af317086b4cdbcdc1675e4b6cf47f61fe9b4
rbn42 <bl100@students.waikato.ac.nz>
2016-07-16 13:25:49 +1200

    auto commit

---
+++ bin/game/valley.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+cd /opt/unigine-valley/bin
+
+./valley_x64   \
+    -project_name Valley   \
+    -data_path ../   \
+    -engine_config ../data/valley_1.0.cfg   \
+    -system_script valley/unigine.cpp   \
+    -sound_app openal   \
+    -video_app opengl   \
+    -video_multisample 3   \
+    -video_fullscreen 0   \
+    -extern_define ,RELEASE,LANGUAGE_EN,QUALITY_ULTRA   \
+    -extern_plugin ,GPUMonitor   \
+    -video_mode 6   \
+
+    #-video_mode 5   \ 6是1980x1080
