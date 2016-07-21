ce430027920a81da9bfe5faa8cf227c2f9b5f070
rbn42 <bl100@students.waikato.ac.nz>
2016-07-21 14:04:57 +1200

    auto commit

---
+++ bin/add_youtube_to_mpd.sh
@@ -0,0 +1,3 @@
+#!/bin/bashuptreJV0hZQ
+#uptreJV0hZQ
+mpc add $(youtube-dl -g -f140 "$1")
---
+++ extra/youtube_mpd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mpc clear
+bash ~/bin/add_youtube_to_mpd.sh "p2vxlZkfW-Y" #Record of Lodoss War - opening eternity
+bash ~/bin/add_youtube_to_mpd.sh "eJiKEy4rsvA" #Record of Lodoss War - opening  ?
+mpc play
+
