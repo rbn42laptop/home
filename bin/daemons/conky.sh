7d0fe91ad798d49ed20e053503433007e7b1a4f0
rbn42 <bl100@students.waikato.ac.nz>
2016-07-30 16:52:49 +1200

    auto commit

--- bin/daemons/conky.sh
+++ bin/daemons/conky.sh
@@ -4,15 +4,19 @@
 #conky -c ~/config/rc/conky
 pkill conky
 sleep 1
+export CORNER=top_right
+export CORNER=bottom_right
 #conky -c ~/config/rc/conky/conky_panel &
 #--conky -c ~/config/rc/conky/top_right/mpd &
 #conky -c ~/config/rc/conky/top_left/mpd &
 #conky -c ~/config/rc/conky/bottom_left/mpd &
 
-conky -c ~/config/rc/conky/top_right/graph_large_top &
-sleep 1
-conky -c ~/config/rc/conky/top_right/graph_large_top_num &
+#conky -c ~/config/rc/conky//graph_large_top &
+#conky -c ~/config/rc/conky/top_right/graph_large_top_num &
 
+conky -c ~/config/rc/conky/$CORNER/graph_large &
+sleep 1
+conky -c ~/config/rc/conky/$CORNER/graph_large_num &
 
 #conky -c ~/config/rc/conky/bottom_right/graph_large &
 #sleep 1
